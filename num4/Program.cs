cb5e9923ca8066acb5ddb20bf395177e66240432
RGolem <kisel2012@inbox.ru>
2022-02-10 01:30:21 +0300

    add Num4

---
+++ num4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+string numstr = Console.ReadLine();
+int num = Convert.ToInt32(numstr);
+int count = 1;
+int ost = 0;
+while (count < (num + 1))
+{
+    ost = count % 2;
+    if (ost == 0 ) 
+    {
+        Console.Write(count.ToString() + " ");
+    }
+    count += 1; 
+}
