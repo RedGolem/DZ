1ab146fff32a4038cb0ed9b19d18dba750d2a11b
RGolem <kisel2012@inbox.ru>
2022-02-10 01:17:46 +0300

    add Num3

---
+++ num3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+string numstr = Console.ReadLine();
+int num = Convert.ToInt32(numstr);
+double ost = num % 2; 
+Console.Write(ost);
+if (ost > 0)
+{
+    Console.WriteLine("Число нечетное");
+} 
+else 
+{   
+    Console.WriteLine("Число четное");
+}
