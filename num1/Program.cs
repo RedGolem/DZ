e22a65a6234833999a154c75947a71cf9b424c41
RGolem <kisel2012@inbox.ru>
2022-02-10 00:43:15 +0300

    add First

---
+++ num1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите первое число и нажмите клавишу Enter: ");
+string a = Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+Console.Write("Введите второе число и нажмите клавишу Enter: ");
+string b = Console.ReadLine();
+int b1 = Convert.ToInt32(b);
+if (a1 == b1)
+    {
+        Console.WriteLine("Числа равны");
+    }
+else
+{
+    if (a1>b1)
+    { 
+      Console.WriteLine("Первое число больше второго");  
+    }
+    else
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
+}
