750e873af3c52420bab88877004a5dffc96c234b
RGolem <kisel2012@inbox.ru>
2022-02-10 18:36:31 +0300

    add Nomer 14

---
+++ num7/Program.cs
@@ -0,0 +1,19 @@
+﻿int Last (int num)
+{
+    int last = num % 10;
+    return last;
+}
+int Proverka (int num)
+{
+    if (num == 0) Console.WriteLine("Последней цифры нет");
+    else Console.Write(num);
+    return num;
+}
+
+
+
+
+Console.Write("Введите трехзначное число и нажмите клавишу Enter: ");
+string numstr = Console.ReadLine();
+int num = Convert.ToInt32(numstr);
+int result = Proverka(Last(num));
