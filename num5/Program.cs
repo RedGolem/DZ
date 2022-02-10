0037d098989fc7109285b2802358dc3792cec8b6
RGolem <kisel2012@inbox.ru>
2022-02-10 14:40:58 +0300

    add Nomer 10

---
+++ num5/Program.cs
@@ -0,0 +1,16 @@
+﻿int Thnum (int num)
+{
+    int thm = num % 100;
+    int th = thm / 10;
+    return th;
+}
+
+
+
+
+Console.Write("Введите первое число и нажмите клавишу Enter: ");
+string numstr = Console.ReadLine();
+int num = Convert.ToInt32(numstr);
+
+int result = Thnum(num);
+Console.WriteLine(result);
